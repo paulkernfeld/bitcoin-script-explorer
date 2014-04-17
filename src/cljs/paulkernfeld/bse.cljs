883bc0165970542b89e82eeb452fd9991239a273
Paul Kernfeld <pk@knewton.com>
2014-04-16 22:32:17 -0400

    Add op descriptions to clojure

--- src/cljs/paulkernfeld/bse.cljs
+++ src/cljs/paulkernfeld/bse.cljs
@@ -33,7 +33,7 @@
    (fn [hex] (parse-int (apply str hex) 16))
    (partition 2 hexes)))   
 
-(defrecord Op [opcode name execute])
+(defrecord Op [opcode name execute description])
 
 (defrecord Parsed [op remaining])
 
@@ -44,6 +44,17 @@
 (defn noop [state] state)
 
 (defn make-op-push [data] (fn [state] (State. (conj (:stack state) data) (:result state))))
+
+(defn make-op-push-description [data]
+  (str
+   "Push "
+   (count data)
+   " bytes onto the stack: "
+   (apply
+    str
+    (map
+     (fn [part] (str "<br>" (to-hex part)))
+     (partition 8 data)))))
 
 (defn op-true [state] (State. (conj (:stack state) [1]) (:result state)))
 
@@ -75,12 +86,12 @@
          (< (count (rest script)) opcode)
        (throw-patched (str "Not enough bytes to push for OP_PUSH_" opcode))
 ;;       (Parsed. (Op. opcode (str "OP_PUSH_" opcode "_" (to-hex (take opcode(rest script)))) (make-op-push (take opcode(rest script)))) (drop opcode (rest script))))
-       (Parsed. (Op. opcode (str "OP_PUSH_" opcode) (make-op-push (take opcode(rest script)))) (drop opcode (rest script))))
-     (= opcode 0x51) (Parsed. (Op. 0x51 "OP_TRUE" op-true) (rest script))
-     (= opcode 0x76) (Parsed. (Op. 0x76 "OP_DUP" op-dup) (rest script))
-     (= opcode 0x88) (Parsed. (Op. 0x88 "OP_EQUALVERIFY" op-equalverify) (rest script))
-     (= opcode 0xa9) (Parsed. (Op. 0xa9 "OP_HASH160" op-hash160) (rest script))
-     (= opcode 0xac) (Parsed. (Op. 0xac "OP_CHECKSIG" op-checksig) (rest script))
+       (Parsed. (Op. opcode (str "OP_PUSH_" opcode) (make-op-push (take opcode(rest script))) (make-op-push-description (take opcode(rest script)))) (drop opcode (rest script))))
+     (= opcode 0x51) (Parsed. (Op. 0x51 "OP_TRUE" op-true "Push the value 0x01 onto the stack") (rest script))
+     (= opcode 0x76) (Parsed. (Op. 0x76 "OP_DUP" op-dup "Push the top item onto the stack") (rest script))
+     (= opcode 0x88) (Parsed. (Op. 0x88 "OP_EQUALVERIFY" op-equalverify "Returns 1 if the inputs are exactly equal, 0 otherwise. Then, marks transaction as invalid if top stack value is not true") (rest script))
+     (= opcode 0xa9) (Parsed. (Op. 0xa9 "OP_HASH160" op-hash160 "Hash the top item on the stack, first with SHA256 then with RIPEMD160.") (rest script))
+     (= opcode 0xac) (Parsed. (Op. 0xac "OP_CHECKSIG" op-checksig "The entire transaction's outputs, inputs, and script are hashed. Returns 1 if signature used by OP_CHECKSIG must be a valid signature for this hash and public key. Otherwise, returns 0.") (rest script))
      :else (throw-patched (str "Unsupported opcode: " (to-hex [opcode]))))))
 
 (defn parse-full [script]
